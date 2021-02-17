463079346090815de2ccd430fe7debaa13d7cca6
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-02-17 15:35:53 -0500

    set up basic collection view

---
+++ Bot-Shop/Controller/NewItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewItemCell.swift
+//  Bot-Shop
+//
+//  Created by X Y on 2/17/21.
+//
+
+import Foundation
+import UIKit
+
+class NewItemCell: UICollectionViewCell {
+    
+    
+}
--- Bot-Shop/Controller/NewOrderViewController.swift
+++ Bot-Shop/Controller/NewOrderViewController.swift
@@ -7,14 +7,57 @@
 import Foundation
 import UIKit
 
-class NewOrderViewController: UIViewController {
+class NewOrderViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 30
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor(red:1.0, green:1.0, blue:1.0, alpha:0.0)
+
+        collectionView.register(NewItemCell.self, forCellWithReuseIdentifier: "cell")
+
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        setViews()
+    }
+    
+    func setViews(){
+
+        self.view.addSubview(collectionView)
+        collectionView.widthAnchor.constraint(equalTo: self.view.layoutMarginsGuide.widthAnchor, multiplier: 1.0).isActive = true
+        collectionView.heightAnchor.constraint(equalTo: self.view.layoutMarginsGuide.heightAnchor, multiplier: 1.0).isActive = true
+        collectionView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        collectionView.centerYAnchor.constraint(equalTo: self.view.layoutMarginsGuide.centerYAnchor).isActive = true
+
     }
 
+}
+extension NewOrderViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewItemCell
+        cell.backgroundColor = .green
+        return cell
+    }
+    //avoid empty space in the cell
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.2, height: collectionView.frame.height/3)
+    }
 
 }
-
