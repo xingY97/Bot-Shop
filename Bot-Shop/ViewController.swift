d35bdabc2780cd8be630fa68719c6703eb91eebb
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-02-17 14:21:34 -0500

    initialized ViewController and navigationController

---
+++ Bot-Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bot-Shop
+//
+//  Created by X Y on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
