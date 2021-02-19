df6cb939f46cf2dba633b72d8bfab03c37089974
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-02-19 14:41:56 -0500

    fixed a problem where users can see the package.

--- Bot-Shop/Controller/PastOrderViewController.swift
+++ Bot-Shop/Controller/PastOrderViewController.swift
@@ -9,18 +9,11 @@ import Foundation
 import UIKit
 
 class PastOrderViewController: UIViewController {
-
+    
     let tableView =  UITableView()
     
-//    let orders = [Order(title: "July 2020", image: UIImage(named: "box")!),
-//                    Order(title: "June 2020", image: UIImage(named: "box")!),
-//                    Order(title: "May 2020", image: UIImage(named: "box")!),
-//                    Order(title: "December 2019", image: UIImage(named: "box")!),
-//                    Order(title: "November 2019", image: UIImage(named: "box")!),
-//                    Order(title: "October 2019", image: UIImage(named: "box")!),
-//                    Order(title: "September 2019", image: UIImage(named: "box")!)]
     var orders: [Order] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,8 +22,9 @@ class PastOrderViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         setUpTableView()
+        getItems()
     }
-
+    
     func setUpTableView(){
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -47,35 +41,35 @@ class PastOrderViewController: UIViewController {
         let robot4 = Item(title: "Excretory", image: UIImage(named: "robot4")!)
         let robot5 = Item(title: "Lymphatic", image: UIImage(named: "robot5")!)
         let robot6 = Item(title: "Nervous", image: UIImage(named: "robot6")!)
-
+        
         let ordersList = [Order(title: "July 2020", image: UIImage(named: "box")!, items: [robot1, robot3]),
-                        Order(title: "June 2020", image: UIImage(named: "box")!, items: [robot2, robot3, robot6]),
-                        Order(title: "May 2020", image: UIImage(named: "box")!, items: [robot4, robot1]),
-                        Order(title: "December 2019", image: UIImage(named: "box")!, items: [robot2, robot5, robot6])
+                          Order(title: "June 2020", image: UIImage(named: "box")!, items: [robot2, robot3, robot6]),
+                          Order(title: "May 2020", image: UIImage(named: "box")!, items: [robot4, robot1]),
+                          Order(title: "December 2019", image: UIImage(named: "box")!, items: [robot2, robot5, robot6])
         ]
-
+        
         for box in ordersList {
             orders.append(box)
         }
     }
-
+    
 }
 
 extension PastOrderViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return orders.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PastOrderCell
-            cell.accessoryType = .disclosureIndicator
-            cell.selectionStyle = .none
-            cell.setBoxContents(box: orders[indexPath.row])
-            cell.textLabel?.text = "\(indexPath.row + 1) \(orders[indexPath.row].title)"
-
+        cell.accessoryType = .disclosureIndicator
+        cell.selectionStyle = .none
+        cell.setBoxContents(box: orders[indexPath.row])
+        cell.textLabel?.text = "\(indexPath.row + 1) \(orders[indexPath.row].title)"
+        
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100.0
     }
